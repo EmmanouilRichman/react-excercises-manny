d74beba4b177648031b42f2612aa4f6fdc7000bf
Manny Richman <erichman966@gmail.com>
2026-02-11 18:35:28 -0500

    1.6 answer

--- lesson1/src/App.jsx
+++ lesson1/src/App.jsx
@@ -1,19 +1,46 @@
 import { useState } from 'react'
-const App = () => {
-  const [ counter, setCounter ] = useState(0)
 
-  const increaseByOne = () => setCounter(counter + 1)
-
-  const setToZero = () => setCounter(0)
-  
-return (
-  <div>
-    <div>{counter}</div>
-    <button onClick={increaseByOne}>plus</button>
-    <button onClick={setToZero}>Zero</button>
-  </div>
-    
-
+const Statistics = ({good,neutral,bad}) => {
+  return(
+    <div>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </div>
   )
 }
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGoodClick = () => {
+    setGood(good + + 1)
+  }
+
+  const handleBadClick = () => {
+    setBad(bad + 1)
+  }
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+
+  }
+
+  return (
+    <div>
+      <h2>Give Feedback</h2>
+      <button onClick={handleGoodClick}>good </button>
+      <button onClick={handleNeutralClick}>neutral </button>
+      <button onClick={handleBadClick}>bad </button>
+
+
+      <h2>Statistics</h2>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
+    </div>
+  )
+}
+
 export default App
