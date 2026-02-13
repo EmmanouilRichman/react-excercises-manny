edef2c2d3d1dbb43aa8d4f4b5379b9f167115667
Manny Richman <erichman966@gmail.com>
2026-02-12 22:48:17 -0500

    1.7 answer

--- lesson1/src/App.jsx
+++ lesson1/src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 
 const Statistics = ({good,neutral,bad,all}) => {
+
   return(
     <div>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
       <p>all {all}</p>
+      <p>average { all > 0 ? (good - bad)/all  : 0 }</p>
+      <p>good {good > 1 ? (good / all) * 100 : 0}%</p>
     </div>
   )
 }
