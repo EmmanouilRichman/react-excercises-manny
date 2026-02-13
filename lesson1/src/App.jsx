1e35b0588b90e85b7d8b9ce1ee1c6e75255b9492
Manny Richman <erichman966@gmail.com>
2026-02-12 22:32:55 -0500

    1.6 answer

--- lesson1/src/App.jsx
+++ lesson1/src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 
-const Statistics = ({good,neutral,bad}) => {
+const Statistics = ({good,neutral,bad,all}) => {
   return(
     <div>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
     </div>
   )
 }
@@ -15,18 +16,29 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
 
   const handleGoodClick = () => {
     setGood(good + + 1)
+    setAll(all + 1)
   }
 
   const handleBadClick = () => {
     setBad(bad + 1)
+    setAll(all + 1)
   }
 
   const handleNeutralClick = () => {
     setNeutral(neutral + 1)
+    setAll(all + 1)
 
+  }
+
+  const handleResetClick = () => {
+    setAll(0)
+    setGood(0)
+    setBad(0)
+    setNeutral(0)
   }
 
   return (
@@ -35,10 +47,11 @@ const App = () => {
       <button onClick={handleGoodClick}>good </button>
       <button onClick={handleNeutralClick}>neutral </button>
       <button onClick={handleBadClick}>bad </button>
+      <button onClick={handleResetClick}>Reset </button>
 
 
       <h2>Statistics</h2>
-      <Statistics good={good} neutral={neutral} bad={bad}/>
+      <Statistics good={good} neutral={neutral} bad={bad} all={all}/>
     </div>
   )
 }
