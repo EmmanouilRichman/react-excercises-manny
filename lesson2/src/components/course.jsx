c2e8f5ea166347a6b6e6385b9b67718642fdfe6f
Manny Richman <erichman966@gmail.com>
2026-02-15 20:02:17 -0500

    answere for 2-2

--- lesson2/src/components/course.jsx
+++ lesson2/src/components/course.jsx
@@ -1,10 +1,19 @@
 const Course = ({course}) => {
+    const totalCalc = (course) =>{
+        let total = 0
+        course.parts.map(part => total+=part.exercises)
+        return (
+            <p><b>total of {total} exercises</b></p>
+        )
+    }
+
     return (
         <div>
-            <h1 key={course.id}>{course.name}</h1>
+            <h1>{course.name}</h1>
             {course.parts.map(part => 
                 <p key={part.id}>{part.name} {part.exercises}</p>
             )}
+            {totalCalc(course)}
         </div>
     )
 }
